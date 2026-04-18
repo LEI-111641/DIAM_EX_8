7eea85476908e01b2dc72b69ca5f6772ed092344
D-Figueira <diogoaf.2004@gmail.com>
2026-04-18 22:50:10 +0100

    h e g

--- process_data.py
+++ process_data.py
@@ -1,6 +1,7 @@
 from votacao.models import Questao, Opcao
 from django.utils import timezone
 
+
 # c) Apagar todas as questões da BD
 def apagar_todas():
     questoes = Questao.objects.all()
@@ -8,12 +9,45 @@ def apagar_todas():
         questao.delete()
     print("Todas as questões foram apagadas.")
 
+
 # d) Mostrar uma questão com as suas opções e votos
 def mostrar_questao(questao):
     print("Questão: " + questao.questao_texto)
     opcoes = questao.opcao_set.all()
     for opcao in opcoes:
         print("  - " + opcao.opcao_texto + " : " + str(opcao.votos) + " votos")
+
+
+# g mostra apenas a questão que tiver o maior número de votos, em caso de igualdade exibe todas as questões empatadas.
+def mostrar_questao_com_mais_votos():
+    questoes = Questao.objects.all()
+    max_votos = 0
+    questoes_com_mais_votos = []
+
+    for questao in questoes:
+        total_votos = sum(opcao.votos for opcao in questao.opcao_set.all())
+        if total_votos > max_votos:
+            max_votos = total_votos
+            questoes_com_mais_votos = [questao]
+        elif total_votos == max_votos:
+            questoes_com_mais_votos.append(questao)
+
+    print("Questão(s) com mais votos (" + str(max_votos) + " votos):")
+    for questao in questoes_com_mais_votos:
+        mostrar_questao(questao)
+
+
+# h Obter o número total de votos registados na BD, iterando sobre todas as opções registadas.
+def total_votos():
+    questoes = Questao.objects.all()
+    total_votos = 0
+
+    for questao in questoes:
+        total_votos += sum(opcao.votos for opcao in questao.opcao_set.all())
+
+    print("Número total de votos registados: " + str(total_votos))
+    return total_votos
+
 
 # --- Testes ---
 # Criar dados temporários para testar
@@ -30,3 +64,23 @@ mostrar_questao(q)
 print("\n=== Teste apagar_todas ===")
 apagar_todas()
 print(Questao.objects.all())
+
+#criar nova questão para testar g) e h)
+q1 = Questao.objects.create(questao_texto="Qual a melhor linguagem de programação?", pub_data=timezone.now())
+q1.opcao_set.create(opcao_texto="Python", votos=10)
+q1.opcao_set.create(opcao_texto="Java", votos=20)
+q1.opcao_set.create(opcao_texto="C++", votos=2)
+
+q2 = Questao.objects.create(questao_texto="Qual a melhor framework?", pub_data=timezone.now())
+q2.opcao_set.create(opcao_texto="Django", votos=15)
+q2.opcao_set.create(opcao_texto="Flask", votos=15)
+q2.opcao_set.create(opcao_texto="Express", votos=5)
+
+# testar g)
+print("\n=== Teste mostrar_questao_com_mais_votos ===")
+mostrar_questao_com_mais_votos()
+
+
+# testar h)
+print("\n=== Teste total_votos ===")
+total_votos()
